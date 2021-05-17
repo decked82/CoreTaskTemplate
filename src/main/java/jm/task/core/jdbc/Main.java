f18a70495fd33bfe75eecaaea3231dedbcd7eb0f
Yun Denis <yundenis1982@gmail.com>
2021-05-17 15:39:15 +0900

    В pom внесли изменения о версии jdbc driver 8.0.23 В Util реализован доступ к базе данных В User переопределен метод toString() В UserDaoJDBCImpl реализованы CRUD операции В UserServiceImpl прописан вызов методов CRUD В main прописан тест доступа к базе данным и CRUD операции

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,23 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Роберт",  "Левандовский", (byte) 32);
         userService.saveUser("Лионель",  "Месси", (byte) 33);
         userService.saveUser("Кевин",  "Де Брёйне", (byte) 29);
         userService.saveUser("Криштиану",  "Роналду", (byte) 36);
         userService.getAllUsers();
-        userService.removeUserById(1);
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        userService.removeUserById(1);
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
 
     }
 }
